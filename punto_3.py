6cc667d4b146de3ba19709b547f0bd513a442018
ezequielpato <ezequiel.salomon@gmail.com>
2021-10-14 12:19:28 -0300

    Se suben archivos hasta el momento

---
+++ punto_3.py
@@ -0,0 +1,18 @@
+# Inicio de mi programa
+mi_variable = 7
+
+if mi_variable == 0:
+  print("Ha ingresado a la primera validación.")
+
+
+elif mi_variable > 3:
+  print("Ha ingresado a la segunda validación.")
+
+elif mi_variable > 6:
+  print("Ha ingresado a la tercer validación.")
+
+
+else:
+  print("No hay coincidencias.")
+
+# Fin del código
---
+++ punto_4.py
@@ -0,0 +1,13 @@
+# Bucle For :
+
+for i in ['A', 'B', 'G', 'K']:
+    print(i)
+
+
+# Bucle While :
+
+condition = 0
+
+while (condition < 5):
+  print("La condición se cumplio.")
+  condition +=1
