fbd77cbb1418a7515fb11ba21d787c3255df9b64
ezequielpato <ezequiel.salomon@gmail.com>
2021-10-18 08:16:47 -0300

    Desarrollando punto 6

---
+++ punto_6.py
@@ -0,0 +1,11 @@
+
+#Input de números solicitados al usuario, definición de variables
+num1 = int(input("Ingrese el primer número ->    "))
+num2 = int(input("Ingrese el segundo número ->   "))
+
+
+#Declaración de la función resta
+def resta(num1, num2):
+    print(num1 + num2) 
+
+resta(2,3)
